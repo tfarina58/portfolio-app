43b9b93051efe49e8f96afb17a75f09b25ed4b7c
Toni <tfarina58@gmail.com>
2025-03-13 14:27:14 +0100

    Initial commit

---
+++ src/app/components/game-card/game-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-card',
+  imports: [],
+  templateUrl: './game-card.component.html',
+  styleUrl: './game-card.component.scss'
+})
+export class GameCardComponent {
+
+}
---
+++ src/app/components/game-modal/game-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-modal',
+  imports: [],
+  templateUrl: './game-modal.component.html',
+  styleUrl: './game-modal.component.scss'
+})
+export class GameModalComponent {
+
+}
