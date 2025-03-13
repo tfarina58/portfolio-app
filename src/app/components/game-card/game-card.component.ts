a6edbc05e21a25beba8700fe8d9f0d7c45fa1e04
Toni <tfarina58@gmail.com>
2025-03-13 14:41:47 +0100

    Moved from app.component to game-card.component

--- src/app/components/game-card/game-card.component.ts
+++ src/app/components/game-card/game-card.component.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-card.component.html',
   styleUrl: './game-card.component.scss'
 })
 export class GameCardComponent {
-
+  expanded: boolean = true;
 }
